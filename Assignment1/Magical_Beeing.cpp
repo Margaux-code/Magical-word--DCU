291653075926ab11db4bf613159aaab9d1f8c663
Margaux-code <margaux.langautier@gmail.com>
2021-10-22 10:35:31 +0100

    First class : Magical_Beeing
    Creation of the first class : Magical_Beeing

---
+++ Assignment1/Magical_Beeing.cpp
@@ -0,0 +1,15 @@
+#include "Magical_Beeing.h"
+
+
+//Constructor
+Magical_Beeing::Magical_Beeing(){}
+Magical_Beeing::Magical_Beeing(std::string name, int age,int quantity_of_magic, int health)
+{
+    m_name =name;
+    m_age=age;
+    m_quantity_of_magic= quantity_of_magic;
+    m_health=health;
+}
+
+//Destructor
+Magical_Beeing::~Magical_Beeing(){}
---
+++ Assignment1/Magical_Beeing.h
@@ -0,0 +1,22 @@
+#ifndef MAGICAL_BEEING_H_INCLUDED
+#define MAGICAL_BEEING_H_INCLUDED
+#include <string>
+class Magical_Beeing
+{
+protected :
+    std::string m_name;
+    int m_age;
+    int m_quantity_of_magic;
+    int m_health;
+public :
+    Magical_Beeing();
+    ~Magical_Beeing();
+    Magical_Beeing(std::string name, int age,int quantity_of_magic, int health);
+
+};
+
+
+
+
+
+#endif // MAGICAL_BEEING_H_INCLUDED
